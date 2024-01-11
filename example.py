7c44311819f757409c29401bab688b2000096b21
Yulin27 <eliane.shiyulin@gmail.com>
2024-01-11 17:06:42 +0100

    upd

--- example.py
+++ example.py
@@ -32,7 +32,7 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-from transformer_LAS import Transformer
+from transformer_LAS import ClassificationTransformer
 from tqdm.auto import tqdm
 
 # Dropout broke in PyTorch 1.11
@@ -149,85 +149,15 @@ valloader = torch.utils.data.DataLoader(
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
 
-class S4Model(nn.Module):
-
-    def __init__(
-        self,
-        d_input,
-        d_output=10,
-        d_model=256,
-        n_layers=4,
-        dropout=0.2,
-        prenorm=False,
-    ):
-        super().__init__()
-
-        self.prenorm = prenorm
-
-        # Linear encoder (d_input = 1 for grayscale and 3 for RGB)
-        self.encoder = nn.Linear(d_input, d_model)
-
-        # Stack S4 layers as residual blocks
-        self.s4_layers = nn.ModuleList()
-        self.norms = nn.ModuleList()
-        self.dropouts = nn.ModuleList()
-        for _ in range(n_layers):
-            self.s4_layers.append(
-                Transformer(d_model, dropout=0., custom_encoder=None, custom_decoder=None)
-            )
-            self.norms.append(nn.LayerNorm(d_model))
-            self.dropouts.append(dropout_fn(dropout))
-
-        # Linear decoder
-        self.decoder = nn.Linear(d_model, d_output)
-
-    def forward(self, x):
-        """
-        Input x is shape (B, L, d_input)
-        """
-        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
-
-        x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
-        for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
-            # Each iteration of this loop will map (B, d_model, L) -> (B, d_model, L)
-
-            z = x
-            if self.prenorm:
-                # Prenorm
-                z = norm(z.transpose(-1, -2)).transpose(-1, -2)
-
-            # Apply S4 block: we ignore the state input and output
-            z, _ = layer(z)
-
-            # Dropout on the output of the S4 block
-            z = dropout(z)
-
-            # Residual connection
-            x = z + x
-
-            if not self.prenorm:
-                # Postnorm
-                x = norm(x.transpose(-1, -2)).transpose(-1, -2)
-
-        x = x.transpose(-1, -2)
-
-        # Pooling: average pooling over the sequence length
-        x = x.mean(dim=1)
-
-        # Decode the outputs
-        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
-
-        return x
 
 # Model
 print('==> Building model..')
-model = S4Model(
+model = ClassificationTransformer(
     d_input=d_input,
     d_output=d_output,
     d_model=args.d_model,
-    n_layers=args.n_layers,
-    dropout=args.dropout,
-    prenorm=args.prenorm,
+    nhead=8,
+    dropout=0.
 )
 
 model = model.to(device)
